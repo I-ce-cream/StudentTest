4ec2dcec1aef2e09c8ba22ef4f2e69b2231631b4
zhangxize <541459574@qq.com>
2021-08-27 16:55:01 +0800

    update 20210827

--- backend/apps/studentserver/serializers.py
+++ backend/apps/studentserver/serializers.py
@@ -3,12 +3,22 @@ from rest_framework import serializers
 # from .models import Studentinfo
 from . import models
 
+
+# # 该方法将外键显示替换
+# class StudentField(serializers.RelatedField):
+#     def to_representation(self, value):
+#         return value.student_no
+
+
 class StudentInfoSerializer(serializers.HyperlinkedModelSerializer):
 # class StudentSerializer(serializers.ModelSerializer):
+    student_no = serializers.CharField(source='student_id.student_no',read_only=True)
+    # # 该方法使用StudentField将外键显示替换
+    # student_id = StudentField(read_only=True)
     class Meta:
         model = models.Studentinfo
         # 指定全部使用 fields = '__all__'
-        fields = ('url', 'student_id', 'student_name', 'student_sex', 'student_age', 'student_date', 'student_class')
+        fields = ('url', 'student_id', 'student_no', 'student_name', 'student_sex', 'student_age', 'student_date', 'student_class')
 
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
